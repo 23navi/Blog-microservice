8ede882c0dd00cd1fc7e0bd026f8ada70e69a3ee
Navi sureka <navisureka23@gmail.com>
2023-04-10 11:28:53 +0530

    added routes for posts

---
+++ posts/index.js
@@ -0,0 +1,31 @@
+const express= require("express");
+const {randomBytes} = require("crypto");
+const bodyParser= require("body-parser");
+
+
+const app= express();
+app.use(bodyParser.json());
+
+const posts={}
+
+
+app.get("/posts",(req,res)=>{
+    res.send(posts);
+
+})
+
+app.post("/posts",(req,res)=>{
+    const id= randomBytes(4).toString();
+    const {title}= req.body;
+    posts[id]={
+        title,id 
+    }
+
+    res.status(201).send(posts[id]);
+
+
+})
+
+app.listen(4000,()=>{
+    console.log("Posts on port 4000")
+})
