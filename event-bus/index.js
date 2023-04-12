a2899948eb38ecbf82af1d0250eaea9d280a090c
Navi sureka <navisureka23@gmail.com>
2023-04-12 16:57:50 +0530

    implemented a basic event broker

---
+++ event-bus/index.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const axios= require("axios");
+const bodyParser = require("body-parser");
+const cors= require("cors")
+const app = express();
+app.use(bodyParser.json());
+app.use(cors())
+
+
+app.post("/events",(req,res)=>{
+
+    axios({
+        method: 'post',
+        url: 'http://127.0.0.1:4000/events',
+        data: req.body.data
+      }).then(r=>{
+        console.log("send")
+    }).catch(e=>{
+        console.log("error");
+        console.log(e.message)
+    });
+
+    axios({
+        method: 'post',
+        url: 'http://127.0.0.1:4001/events',
+        data: req.body.data
+      }).then(r=>{
+        console.log("send")
+    }).catch(e=>{
+        console.log("error");
+    });
+    res.send("Ok");
+})
+
+
+
+
+app.listen(4005, () => {
+    console.log("Event bus on port 4005");
+  });
+  
--- posts/index.js
+++ posts/index.js
@@ -2,6 +2,7 @@ const express= require("express");
 const {randomBytes} = require("crypto");
 const bodyParser= require("body-parser");
 const cors= require("cors")
+const axios = require("axios");
 
 
 const app= express();
@@ -23,10 +24,31 @@ app.post("/posts",(req,res)=>{
         title,id 
     }
 
+    axios({
+        method: 'post',
+        url: 'http://127.0.0.1:4005/events',
+        data: {
+            tyep:"CommentCreated",
+            data:{
+              id,
+              title,
+            }
+        }
+      }).catch(e=>{
+        console.log("error");
+    });
+
+ 
+
     res.status(201).send(posts[id]);
 
 
 })
+
+app.post("/events",(req,res)=>{
+    console.log(req.body)
+    res.send({})
+  })
 
 app.listen(4000,()=>{
     console.log("Posts on port 4000")
